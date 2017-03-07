d1715ac3817f6c138a0baf54b477460904cf766f
cam-shotter <cameronshotter@gmail.com>
2017-03-07 20:27:07 +1300

    FINISHED....finally.....I hope it doesn't break...

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,23 @@
-var x, y = 0
+var x, y = 0;
+var carouselMaster;
 
 function init() {
+  storeVitals()
+  checkForCarousel()
+}
+
+function storeVitals() {
+  carouselMaster = document.getElementById("slideshow-images");
+}
+
+function checkForCarousel() {
   determineScreenSize()
 
   if (x < 769) {
+    console.log("screen is small");
+    removeCarousel()
+  } else {
+    console.log("screen is large");
     replaceCarousel()
   }
 }
@@ -11,23 +25,28 @@ function init() {
 function determineScreenSize() {
   x = innerWidth;
   y = innerHeight;
-  console.log(x, y);
 }
 
-function replaceCarousel() {
-    console.log("screen is xs!");
+function removeCarousel() {
     var carousel = document.getElementById("slideshow-images");
     var carouselInner = carousel.innerHTML;
-    var carouselParent = carousel.parentNode;
+    var carouselParent = document.getElementById("carousel-outer");
     carouselParent.removeChild(carousel);
 
     var div = document.createElement("div");
-    div.className = "carousel-alt";
+    div.id = "carousel-alt";
     div.innerHTML = carouselInner;
     document.getElementById("carousel-outer").appendChild(div)
 
     var newSlides = document.getElementsByClassName('item')
-    for (var i = 1; i < newSlides.length; i++) {
+    for (var i = 0; i < newSlides.length; i++) {
       newSlides[i].className += " active";
     }
+}
+
+function replaceCarousel() {
+  var carouselParent = document.getElementById("carousel-outer");
+  var carouselAlt = document.getElementById("carousel-alt");
+  carouselParent.removeChild(carouselAlt);
+  carouselParent.appendChild(carouselMaster);
 }
